14e88905a53d3d330d86ba86dfe5640eea0d7ba4
thipham2018 <trangphamtmp@gmail.com>
2024-01-09 10:43:14 -0500

    math

--- test.cpp
+++ test.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 
 int main()
 {
-  cout << sqrt(64) << "\n";
-  cout << round(2.6) << "\n";
-  cout << log(2) << "\n";
+  if (18 > 9)
+  {
+    cout << "print";
+  }
   return 0;
 }
