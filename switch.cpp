e5325099234fcfc4bf85fb519a6d586d8cac884b
thipham2018 <trangphamtmp@gmail.com>
2024-01-09 11:08:08 -0500

    switch and case break

---
+++ switch.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  string firstName = "Thi ";
+  string lastName = "Pham";
+  string fullName = firstName.append(lastName);
+  cout << fullName;
+  return 0;
+}
