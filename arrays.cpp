5d79d2630ba237aa7cdb5eb9564e573a60b4cc4a
thipham2018 <trangphamtmp@gmail.com>
2024-01-11 14:29:43 -0500

    Function

--- arrays.cpp
+++ arrays.cpp
@@ -1,17 +1,14 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
+void myFunction()
+{
+  cout << "I just got executed!\n";
+}
 int main()
 {
-  string food = "Pizza"; // a string variable
-  string *ptr = &food;   // A pointer variable that stores the address of food
-
-  // output the value of food
-  cout << food << "\n";
-  // output the memory address of food
-  cout << &food << "\n";
-  // Output the memory address of food with the pointer
-  cout << ptr << "\n";
+  myFunction();
+  myFunction();
+  myFunction();
   return 0;
 }
