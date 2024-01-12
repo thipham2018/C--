8badaa55e9406b06f3df7cbcc6d282d9aa22c0e8
thipham2018 <trangphamtmp@gmail.com>
2024-01-12 11:20:16 -0500

    function overloading

--- test_function.cpp
+++ test_function.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
-#include <string>
 using namespace std;
-
-void myFuction(string fname)
-{ // fname is called a parameter when it passed to the function, it is called an arguments
-  cout << fname << "Refsnes\n";
+// int myFunction(int x);
+// float myFunctiion(float x);
+// double myFunction(double x, double y);
+// multiple functions can have the same name with different parameters
+int plusFuncInt(int x, int y)
+{
+  return x + y;
+}
+double plusFuncDouble(double x, double y)
+{
+  return x + y;
 }
 int main()
 {
-  // Lan, Jenny, Anna are called Arguments
-  myFuction("Lan");
-  myFuction("Jenny");
-  myFuction("Ana");
+  int myNum1 = plusFuncInt(8, 5);
+  double myNum2 = plusFuncDouble(4.3, 6.26);
+  cout << "int: " << myNum1 << "\n";
+  cout << "Double: " << myNum2;
   return 0;
 }
